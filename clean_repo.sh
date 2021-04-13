e1ba9421e8211f02d3cf5dee3bdc09c9cb4caa10
ThePoorPilot <78394939+ThePoorPilot@users.noreply.github.com>
2021-04-12 22:43:44 -0400

    Update clean_repo.sh

--- clean_repo.sh
+++ clean_repo.sh
@@ -6,12 +6,12 @@ cd ./cleaning
 echo "Printing list of all pkgs uploaded to GitHub"
 curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/ThePoorPilot/manjaro-removed-pkgs/releases 2>/dev/null | grep '"name":' | sed -n -e 's/        "name": "//p' | sed -n -e 's/",//p' | sed 's/manjaro-removed-pkgs.*//g' | sed '/^[[:space:]]*$/d' > uploaded_pkgs.txt
 
-echo "Downloading manjaro-removed-pkgs repo database"
-wget -q https://github.com/ThePoorPilot/manjaro-removed-pkgs/releases/download/x86_64/manjaro-removed-pkgs.db
+#echo "Downloading manjaro-removed-pkgs repo database"
+#wget -q https://github.com/ThePoorPilot/manjaro-removed-pkgs/releases/download/x86_64/manjaro-removed-pkgs.db
 echo "extracting manjaro-removed-pkgs database"
 mkdir ./manjaro-removed-pkgs
 {
-tar -zxf ./manjaro-removed-pkgs.db -C ./manjaro-removed-pkgs
+tar -zxf ../packages/manjaro-removed-pkgs.db -C ./manjaro-removed-pkgs
 } &> /dev/null
 awk '/%FILENAME%/{getline; print}' ./manjaro-removed-pkgs/*/desc > ./database_pkgs.txt
 
