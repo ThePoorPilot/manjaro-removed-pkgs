3558bdbe6a702d95b7f53f4151d2e8fa020b8a10
ThePoorPilot <ios8jailbreakpangu@gmail.com>
2021-04-18 18:32:41 -0400

    A few more fixes for actions

--- repo-remove.sh
+++ repo-remove.sh
@@ -8,3 +8,6 @@ mapfile -t myArray < ../repo-remove.txt
 while IFS= read -r line;
 do repo-remove ./manjaro-removed-pkgs.db.tar.gz "$line";
 done < ../repo-remove.txt
+
+cp ./manjaro-removed-pkgs.db.tar.gz ./manjaro-removed-pkgs.db
+cp ./manjaro-removed-pkgs.files.tar.gz ./manjaro-removed-pkgs.files
